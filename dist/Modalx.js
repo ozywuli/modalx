dc3d324d386566f26e4ad838aff1146a07029bbe
ozywuliwq <ozy.wu.li@gmail.com>
2018-01-14 16:45:33 -0800

    first commit

---
+++ dist/Modalx.js
@@ -0,0 +1,28 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Modalx = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+/**
+ * OnToggle.js
+ * @author Ozy Wu-Li - @ousikaa
+ * @description Toggle DOM element state
+ */
+
+// https://github.com/jquery-boilerplate/jquery-patterns/blob/master/patterns/jquery.basic.plugin-boilerplate.js
+
+// the semi-colon before the function invocation is a safety
+// net against concatenated scripts and/or other plugins
+// that are not closed properly.
+// the anonymous function protects the `$` alias from name collisions
+;(function ($, window, document, undefined) {
+    var pluginName = 'OnToggle';
+
+    console.log('modalx');
+
+    /*------------------------------------*\
+      EXPORT OPTIONS
+    \*------------------------------------*/
+    module.exports = Modalx;
+})(jQuery, window, document);
+
+},{}]},{},[1])(1)
+});
---
+++ src/Modalx.js
@@ -0,0 +1,24 @@
+/**
+ * OnToggle.js
+ * @author Ozy Wu-Li - @ousikaa
+ * @description Toggle DOM element state
+ */
+
+// https://github.com/jquery-boilerplate/jquery-patterns/blob/master/patterns/jquery.basic.plugin-boilerplate.js
+
+// the semi-colon before the function invocation is a safety
+// net against concatenated scripts and/or other plugins
+// that are not closed properly.
+// the anonymous function protects the `$` alias from name collisions
+;(function( $, window, document, undefined ) {
+    let pluginName = 'OnToggle';
+
+    console.log('modalx');
+
+
+    /*------------------------------------*\
+      EXPORT OPTIONS
+    \*------------------------------------*/
+    module.exports = Modalx;
+
+})( jQuery, window , document );
